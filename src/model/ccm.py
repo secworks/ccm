da0a0f5726b1956b36b88ff309ebc764e9e6bc83
Joachim Strömbergson <joachim@secworks.se>
2017-04-16 20:29:00 +0200

    Adding initial version of ccm python moodel.

---
+++ src/model/ccm.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#=======================================================================
+#
+# ccm.py
+# ------
+# Simple, pure Python, CCM implementation using the
+# word based model of the AES cipher with support for 128 and
+# 256 bit keys. Test vectors used in the tests are from the SP 800-38C.
+#
+#
+#
+# Author: Joachim Strömbergson
+# Copyright (c) 2017, Secworks Sweden AB
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or
+# without modification, are permitted provided that the following
+# conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in
+#    the documentation and/or other materials provided with the
+#    distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+#=======================================================================
+
+#-------------------------------------------------------------------
+# Python module imports.
+#-------------------------------------------------------------------
+import sys
+
+# Note this assumes that the aes implementation is either in the
+# same dir or symlinked.
+from aes import *
+
+
+#-------------------------------------------------------------------
+# test_cmac()
+#
+#-------------------------------------------------------------------
+def test_ccm():
+    print("Testing complete ccm:")
+    print("---------------------")
+
+
+#-------------------------------------------------------------------
+# main()
+#
+# If executed tests the cmac function and its subfunctions.
+#-------------------------------------------------------------------
+def main():
+    print("Testing the CCM-AES mode")
+    print("========================")
+    print
+
+    test_ccm()
+
+
+#-------------------------------------------------------------------
+# __name__
+# Python thingy which allows the file to be run standalone as
+# well as parsed from within a Python interpreter.
+#-------------------------------------------------------------------
+if __name__=="__main__":
+    # Run the main function.
+    sys.exit(main())
+
+#=======================================================================
+# EOF ccm.py
+#=======================================================================
